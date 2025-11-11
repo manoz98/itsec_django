54254dac71767e83a86b23d96259fd00c384a2bc
manu <manozedu@gmail.com>
2025-11-11 15:52:51 +0545

    updated with ip and password

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
-
+import os
+import socket
 
 def index(request):
-    return HttpResponse("<h1> Hello IT Security Nepal <h1> new line check blah blah ")
+    hostname = socket.gethostname()
+    try:
+        ip_address = socket.gethostbyname(hostname)
+    except socket.gaierror:
+        ip_address = "Unable to determin IP address"
+    imp = os.environ.get("IMP_Text","Not Set Yet")
+    return HttpResponse(f"<h1> Hello IT Security Nepal <h1> new line check blah blah <br> Hostname: {hostname} <br> IP Address: {ip_address} <br> <br> Important text is {imp}")
